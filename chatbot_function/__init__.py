bc661d21fff62506f24ba7f8814c429e16098711
sourimmitramol <sourim.mitra@molgroup.com>
2025-07-04 18:51:31 +0530

    Update __init__.py

--- chatbot_function/__init__.py
+++ chatbot_function/__init__.py
@@ -1,6 +1,13 @@
 import azure.functions as func
+import time
+import requests
+from datetime import datetime, timezone
+from openai import AzureOpenAI
+import os
 import json
+from typing import Optional, Union, Any, Dict
 from final_chatbot import query_chatbot
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
@@ -14,7 +21,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 mimetype="application/json"
             )
 
-        answer = query_chatbot(user_question)
+        #answer = query_chatbot(user_question)
+        answer = "test"
 
         return func.HttpResponse(
             json.dumps({"question": user_question, "answer": answer}),
