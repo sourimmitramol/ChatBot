2bd03a9c5d914fd95660129e6756ce39b6d3893f
sourimmitramol <sourim.mitra@molgroup.com>
2025-07-04 19:38:41 +0530

    Update __init__.py

--- chatbot_function/__init__.py
+++ chatbot_function/__init__.py
@@ -2,11 +2,11 @@ import azure.functions as func
 # import time
 import requests
 # from datetime import datetime, timezone
-from openai import AzureOpenAI
+# from openai import AzureOpenAI
 # import os
 import json
 # from typing import Optional, Union, Any, Dict
-# from final_chatbot import query_chatbot
+from final_chatbot import query_chatbot
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
